348a30e0273343934af0624fe8e78ebedc486f50
Nikita Pekin <nikitapekin@gmail.com>
2013-11-15 04:24:29 -0500

    Implement basic Marble integration for SQL testing

---
+++ src/main/java/in/nikitapek/insightweb/SQL.java
@@ -0,0 +1,31 @@
+package in.nikitapek.insightweb;
+
+import in.nikitapek.marble.sql.MetadataQueries;
+import in.nikitapek.marble.sql.SQLConnector;
+
+public class SQL {
+    public static final String columnNames = "`id`, `x`, `y`, `z`";
+    public static final String databaseName = "marbleTest";
+    public static final String tableName = "data";
+
+    public static void initializeSQL() {
+        String dbhostname = System.getProperty("dbhostname");
+        String dbusername = System.getProperty("dbusername");
+        String dbpassword = System.getProperty("dbpassword");
+        String dbport = System.getProperty("dbport");
+
+        if (dbhostname == null || dbusername == null || dbpassword == null || dbport == null) {
+            throw new RuntimeException("Invalid configuration. Make sure to set dbhostname, dbusername, dbpassword, and dbport!");
+        }
+
+        SQLConnector.initializeSQL(dbhostname, dbusername, dbpassword, dbport, databaseName, true);
+
+        // Drop the database in case it remains from a previous test case.
+        MetadataQueries.dropDatabase(databaseName);
+    }
+
+    public static void endTest() {
+        MetadataQueries.dropDatabase(databaseName);
+        SQLConnector.disconnect();
+    }
+}
