996c6ce061f59a62305f3675f0c97eb8d7d46083
Nikita Pekin <nikitapekin@gmail.com>
2013-11-27 14:05:34 -0500

    Fix improper mapping breaking static file linking

--- src/main/java/in/nikitapek/insightweb/servlet/LoginFailedServlet.java
+++ src/main/java/in/nikitapek/insightweb/servlet/LoginFailedServlet.java
@@ -15,16 +15,6 @@ public class LoginFailedServlet extends HttpServlet {
         String status = "<h1>Login Failed</h1>";
         request.setAttribute("status", status);
 
-        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
-        String exceptionType = (String) request.getAttribute("javax.servlet.error.exception_type");
-        String errorMsg = (String) request.getAttribute("javax.servlet.error.message");
-
-        System.out.println(statusCode);
-        System.out.println(exceptionType);
-        System.out.println(errorMsg);
-        System.out.println(request.getUserPrincipal());
-        System.out.println(request.getRemoteUser());
-
         request.getRequestDispatcher(page).forward(request, response);
     }
 }
